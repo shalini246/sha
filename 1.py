18102380055f24f8f3bf067fd07b8a19f5cce5b2
shalini246 <41140767+shalini246@users.noreply.github.com>
2018-07-12 13:06:32 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 n=int(input())
- if(n==0):
-   print("zero")
- elif(n>0):
-   print("positive")
- else:
-   print("negative")
+if(n==0):
+  print("zero")
+elif(n>0):
+  print("positive")
+else:
+  print("negative")
